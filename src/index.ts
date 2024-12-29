9e52fa7ee1b9f516cfbc89e1e4521c3253fb88d9
Shigma <shigma10826@gmail.com>
2024-12-29 10:38:32 +0000

    feat: basic implementation

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,49 @@
-import { Context, Schema } from 'koishi'
+import { Context, remove, Schema, Service } from 'koishi'
+import {} from '@koishijs/plugin-server'
+
+declare module 'koishi' {
+  interface Context {
+    eventServer: EventServer
+  }
+}
 
 export const name = 'event-server'
 
-export interface Config {}
+class EventServer extends Service {
+  static inject = ['server']
 
-export const Config: Schema<Config> = Schema.object({})
+  patterns: RegExp[] = []
 
-export function apply(ctx: Context) {
-  // write your plugin here
+  constructor(ctx: Context, config: EventServer.Config) {
+    super(ctx, 'eventServer')
+
+    const layer = ctx.server.ws(config.path)
+
+    ctx.on('internal/event', (type, name, args, thisArg) => {
+      if (!this.patterns.some(pattern => pattern.test(name))) return
+      for (const client of layer.clients) {
+        client.send(JSON.stringify({ name, args }))
+      }
+    })
+  }
+
+  register(name: string) {
+    return this.ctx.effect(() => {
+      const regexp = new RegExp(name.replace(/\*/g, '.*'))
+      this.patterns.push(regexp)
+      return () => remove(this.patterns, regexp)
+    })
+  }
 }
+
+namespace EventServer {
+  export interface Config {
+    path: string
+  }
+  
+  export const Config: Schema<Config> = Schema.object({
+    path: Schema.string().default('/events'),
+  })
+}
+
+export default EventServer
